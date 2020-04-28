f1372154cbb6bf88232e98290bb63dc086b9857c
William May <williamcmay@live.com>
2020-04-28 18:27:10 -0400

    Organize and update R packages

--- install_packages.R
+++ install_packages.R
@@ -1,21 +1,39 @@
-basic_packages = c(
-    'tidyverse',
-    "devtools", "roxygen2", # package development tools
-    "rmarkdown", 'rticles', # generate PDFs
-    ## "rstan", "rjags", # Bayesian modeling
-    ## "testthat", # unit testing
-    "shiny", # web server
-    "pscl", "basicspace", "wnominate", "rsunlight" # poli sci
+dev_pkgs = c(
+    "devtools",
+    "remotes",
+    "roxygen2",
+    "testthat",
+    "usethis"
 )
-install.packages(basic_packages)
-
-# gEcon
-## install.packages(c("Matrix", "MASS", "nleqslv", "Rcpp"))
-# download package from here:
-# http://gecon.r-forge.r-project.org/download.html
-# install with `R CMD INSTALL gEcon_x.x.x.tar.gz`
-
-# Bayesian Macroeconomics in R
-## install.packages(c("RcppArmadillo", "ggplot2", "doParallel"))
-## library(devtools)
-## install_github("kthohr/BMR")
+util_pkgs = c(
+    "bookdown",
+    "DT",
+    "dygraphs",
+    "ggplot2",
+    "jsonlite",
+    "leaflet",
+    "lubridate",
+    "magrittr",
+    "plotly",
+    "reshape2",
+    "rmarkdown",
+    "RPostgreSQL",
+    "rticles",
+    "rvest",
+    "shiny",
+    "tidyverse"
+)
+stats_pkgs = c(
+    "forecast",
+    "mgcv",
+    "prophet",
+    "rjags",
+    "rstan"
+)
+polisci_pkgs = c(
+    "basicspace",
+    "oc",
+    "pscl",
+    "wnominate"
+)
+install.packages(c(dev_pkgs, util_pkgs, stats_pkgs, polisci_pkgs))
