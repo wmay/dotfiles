5b498cfbe5f860fd07518f12d31180d6fdf38a88
William May <williamcmay@live.com>
2020-04-28 02:35:52 -0400

    Add link for config.fish

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -8,4 +8,5 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/gitignore_global ~/.gitignore_global
 ln -s ${BASEDIR}/emacs ~/.emacs
+ln -s ${BASEDIR}/config.fish ~/.config/fish/config.fish
 ln -s ${BASEDIR}/hidden ~/.hidden
