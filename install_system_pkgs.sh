1153565bd8bc3f51763649a83109b4b9fe8c9971
William May <wmay@hey.com>
2023-08-15 10:42:29 -0400

    Fix firefox snap installation issue

--- install_system_pkgs.sh
+++ install_system_pkgs.sh
@@ -18,6 +18,10 @@ echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
+
+Package: firefox
+Pin: version 1:1snap1-0ubuntu2
+Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 sudo apt install firefox
 
