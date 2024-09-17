27de640351b2ea5d759dbd14ccb1049b5753b54d
William May <wmay@hey.com>
2024-09-16 20:45:14 -0400

    Update R installation

--- install_system_pkgs.sh
+++ install_system_pkgs.sh
@@ -2,7 +2,10 @@
 # Set up an Ubuntu, or Ubuntu derivative, desktop
 # don't use sudo!!
 
-# Add the CRAN Ubuntu repository
+# Add the CRAN Ubuntu repository. Following
+# https://cran.r-project.org/bin/linux/ubuntu/
+sudo apt update -qq
+sudo apt install --no-install-recommends software-properties-common dirmngr
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc |\
     sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
