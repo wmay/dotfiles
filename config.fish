741d942a8c265c98e0366a436a9163d9e387ac05
William May <wmay@hey.com>
2025-03-27 02:41:08 -0400

    Make changes for miniforge

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ set fish_greeting
 function fish_greeting; end
 set -g theme_color_scheme base16-dark
 set -g theme_date_timezone America/New_York
+set -g theme_nerd_fonts yes
 
 # package dev
 set -g DEBFULLNAME "William May"
@@ -16,3 +17,18 @@ set -g R_REMOTES_UPGRADE "never"
 # Ruby exports
 set -gx GEM_HOME $HOME/gems
 fish_add_path $HOME/gems/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/will/miniforge3/bin/conda
+    eval /home/will/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/will/miniforge3/etc/fish/conf.d/conda.fish"
+        . "/home/will/miniforge3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/will/miniforge3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+conda config --set changeps1 False
