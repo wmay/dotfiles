8edcf3e21ed00286d13a6bc0978bab30218e3e36
William May <wmay@hey.com>
2025-04-07 16:55:43 -0400

    Fix tensorflow issue

--- config.fish
+++ config.fish
@@ -16,3 +16,8 @@ set -g R_REMOTES_UPGRADE "never"
 # Ruby exports
 set -gx GEM_HOME $HOME/gems
 fish_add_path $HOME/gems/bin
+
+# Fix tensorflow 2.16.1. See
+# https://github.com/tensorflow/tensorflow/issues/63362#issuecomment-1988630226
+set NVIDIA_PACKAGE_DIR /home/wmay/.local/lib/python3.10/site-packages/nvidia
+for dir in $NVIDIA_PACKAGE_DIR/*/lib; set -gx LD_LIBRARY_PATH $dir $LD_LIBRARY_PATH; end
