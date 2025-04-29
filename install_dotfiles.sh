6b8c48c397d2f103060e4e3091b820fe944585d2
William May <wmay@hey.com>
2025-04-29 17:12:53 -0400

    Add firefox userContent.css

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -15,3 +15,6 @@ ln -sf ${BASEDIR}/.hidden ~/.hidden
 ln -sf ${BASEDIR}/.ipython/profile_default/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 ln -sf ${BASEDIR}/.ipython/profile_default/startup/startup.ipy ~/.ipython/profile_default/startup/startup.ipy
 ln -sf ${BASEDIR}/.condarc ~/.condarc
+FIREFOX_PROFILE_DIR=$(find ~/.mozilla/firefox -name '*.default-release' -type d | head -n 1)
+mkdir -p ${FIREFOX_PROFILE_DIR}/chrome
+ln -sf ${BASEDIR}/.mozilla/firefox/profile.default/chrome/userContent.css ${FIREFOX_PROFILE_DIR}/chrome/userContent.css
